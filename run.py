24a985be08b2f9f3b2d888c1231b66eb5e9bab81
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-07-31 21:57:28 +0000

    Add sales data to sales worksheet

--- run.py
+++ run.py
@@ -28,7 +28,10 @@ data = sales.get_all_values()
 
 def get_sales_data():
     """
-    Get sales figures input from the user
+    Get sales figures input from the user.
+    Run a while loop to cllect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
@@ -44,6 +47,7 @@ def get_sales_data():
             break
 
     return sales_data
+
 
 def validate_data(values):
     """ Inside the try, converts all string values into integers.
@@ -63,5 +67,17 @@ def validate_data(values):
     return True
 
 
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided.
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet('sales')
+    sales_worksheet.append_row(data)
+    print('Sales worksheet updated succesfully.\n')
+    
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
 
